6dad199db69781cd60270faa48f4c524163a36ab
archanawwww <archanabisht2005@gmail.com>
2024-11-12 22:29:56 +0530

    initial commit of guessing numbers

---
+++ kukii.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int number, guess, attempts = 0;
+    
+    // Seed the random number generator
+    srand(time(0));
+    
+    // Generate a random number between 1 and 100
+    number = rand() % 100 + 1;
+    
+    printf("Welcome to the Number Guessing Game!\n");
+    printf("I have selected a number between 1 and 100.\n");
+    
+    // Loop until the player guesses the number
+    do {
+        printf("Enter your guess: ");
+        scanf("%d", &guess);
+        attempts++;
+        
+        if (guess > number) {
+            printf("Too high! Try again.\n");
+        } else if (guess < number) {
+            printf("Too low! Try again.\n");
+        } else {
+            printf("Congratulations! You've guessed the number %d in %d attempts.\n", number, attempts);
+        }
+    } while (guess != number);
+    
+    return 0;
+}
